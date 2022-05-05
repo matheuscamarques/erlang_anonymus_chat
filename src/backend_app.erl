c3327178c3a172ca7cc78c4f54d6c5c86afe4d46
matheuscamarques <matheuscamarques@gmail.com>
2022-05-05 19:11:37 -0300

    added port corret

--- src/backend_app.erl
+++ src/backend_app.erl
@@ -24,7 +24,7 @@ start(_StartType, _StartArgs) ->
     ]),
     {ok, _} = cowboy:start_clear(
         hello_listener,
-        [{port, 80}],
+        [{port, 8080}],
         #{env => #{dispatch => Dispatch}}
     ),
     backend_sup:start_link().
